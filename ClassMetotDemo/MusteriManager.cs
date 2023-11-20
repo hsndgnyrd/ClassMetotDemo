0cb4f5753169c26928c653bc75463c26d6d6c760
hsndgnyrd <hasandugunyurdu27@gmail.com>
2023-11-20 22:41:27 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi: " + musteri.Name + " " + musteri.LastName);
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi. " + musteri.Name + " " + musteri.LastName);
+        }
+
+        public void MusteriListele() 
+        {
+            Console.WriteLine("Müşteriler Listelendi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using ClassMetotDemo;
+using System;
+
+namespace ClassMetodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Name = "Hasan";
+            musteri1.Id = 1;
+            musteri1.LastName = "Düğünyurdu";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Name = "Elon";
+            musteri2.Id = 2;
+            musteri2.LastName = "Musk";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id + " " + musteri.Name + " " + musteri.LastName);
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriSil(musteri1);
+            musteriManager.MusteriListele();
+
+        }
+    }
+}
